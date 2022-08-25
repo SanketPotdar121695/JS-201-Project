2f699db8419b868c417b635bcb87f37bfea6eff9
subhankarroy612 <subhankarroy612@gmail.com>
2022-08-25 20:24:23 +0530

    day-3 task completed

--- features.js
+++ features.js
@@ -2,12 +2,24 @@
 let navbar = document.querySelector('header');
 
 window.addEventListener('scroll', (e) => {
-  
-    if(window.scrollY>10){
-       navbar.style.backgroundColor = "white"
-    }else if(window.scrollY<10){
+
+    if (window.scrollY > 10) {
+        navbar.style.backgroundColor = "white"
+    } else if (window.scrollY < 10) {
         navbar.style.backgroundColor = "#f9e0fa"
     }
-    console.log(window.scrollY);
-    
-})
+
+});
+
+let img = document.querySelector('#video>img');
+img.addEventListener('click', () => {
+
+    let video = document.querySelector('#video');
+    video.innerHTML = ""
+
+    let a = document.createElement('iframe');
+    a.src = "video1.mp4";
+
+    video.append(a)
+});
+
